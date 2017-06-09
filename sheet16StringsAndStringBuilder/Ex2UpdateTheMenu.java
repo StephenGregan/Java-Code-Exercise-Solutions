b9542a38cf399f2ab15e86b300070b7396cc7525
StephenGregan <Grego@live.ie>
2017-06-09 12:26:28 +0100

    New Code

---
+++ sheet16StringsAndStringBuilder/Ex2UpdateTheMenu.java
@@ -0,0 +1,21 @@
+package sheet16StringsAndStringBuilder;
+
+public class Ex2UpdateTheMenu {
+
+	public static void main(String[] args) {
+	
+		String todaysMenu = "Wednesday's Menu : Roast turkey with homemade turkey gravy, potatoes and veg of the day";
+		
+		String text = "Thursday";
+		text = todaysMenu.replace("Wednesday's", "Thursday's");
+		System.out.println(todaysMenu.replace("Wednesday's", "Thursday's")); // A
+		
+		String text1 = "beef";
+		text1 = todaysMenu.replace("turkey","beef");
+		System.out.println(todaysMenu.replace("turkey", "beef")); // B
+		
+		
+		
+	}
+
+}
